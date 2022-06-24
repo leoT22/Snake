4bc517838772c39aac07e9ef2f54f24b4211ef79
Leonardo Telles <leonardotelles1204@gmail.com>
2022-06-24 18:00:36 -0300

    Adicionando movimentação

--- snake.py
+++ snake.py
@@ -10,6 +10,48 @@ altura = 480
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Snake')
+relogio = pygame.time.Clock()
+
+while True:
+    relogio.tick(30)
+    tela.fill((255,255,255))
+    
+    mensagem = f'Pontos: {pontos}'
+    texto_formatado = fonte.render(mensagem, True, (0,0,0))
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle = -velocidade
+                    y_controle = 0
+            if event.key == K_d:
+                if x_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = velocidade
+                    y_controle = 0
+            if event.key == K_w:
+                if y_controle == velocidade:
+                    pass
+                else:
+                    y_controle = -velocidade
+                    x_controle = 0
+            if event.key == K_s:
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    y_controle = velocidade
+                    x_controle = 0
+
+    x_cobra = x_cobra + x_controle
+    y_cobra = y_cobra + y_controle
 
 x_cobra = int(largura/2) 
 y_cobra = int(altura/2)
