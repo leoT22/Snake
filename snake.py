4065c5274fcd17280a5346afca5159913da00a28
Leonardo Telles <leonardotelles1204@gmail.com>
2022-06-24 17:58:26 -0300

    Criando personagens

--- snake.py
+++ snake.py
@@ -11,4 +11,10 @@ altura = 480
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Snake')
 
+x_cobra = int(largura/2) 
+y_cobra = int(altura/2)
+
+cobra = pygame.draw.rect(tela, (0,255,0), (x_cobra,y_cobra,20,20))
+maca = pygame.draw.rect(tela, (255,0,0), (x_maca,y_maca,20,20))
+
 pygame.display.update()
